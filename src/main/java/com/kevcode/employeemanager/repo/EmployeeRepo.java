5b5ca1d6c26f1286b1afe42522cad4450199d9e1
Kevin-Niv <80486302+Kevin-Niv@users.noreply.github.com>
2021-03-11 23:19:17 +0300

    Add files via upload

---
+++ src/main/java/com/kevcode/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,13 @@
+package com.kevcode.employeemanager.repo;
+
+import com.kevcode.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
