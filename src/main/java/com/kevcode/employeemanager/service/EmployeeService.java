2df01099e67d9264b7b211b181604f6402e69e44
Hanan-Hussein <hanhuss035@gmail.com>
2021-03-12 10:02:16 +0300

    resolve

--- src/main/java/com/kevcode/employeemanager/service/EmployeeService.java
+++ src/main/java/com/kevcode/employeemanager/service/EmployeeService.java
@@ -20,7 +20,7 @@ public class EmployeeService {
     }
 
     public Employee addEmployee(Employee employee){
-        employee.getEmployeeCode(UUID.randomUUID().toString());
+      //  employee.getEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
     }
 
